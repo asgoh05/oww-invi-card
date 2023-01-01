cb70438e07cdb7470bbf3ff7513cf07a06afa47a
asgoh05 <asgoh05@gmail.com>
2023-01-01 21:50:51 +0900

    update calendar

--- src/components/CalendarPage/CalendarPage.jsx
+++ src/components/CalendarPage/CalendarPage.jsx
@@ -4,19 +4,19 @@ import "react-calendar/dist/Calendar.css";
 import style from "./CalendarPage.module.css";
 
 export default function CalendarPage() {
-  const date = new Date(2023, 2, 26);
   return (
     <div className={style.container} data-aos="fade-up" ata-aos-duration="3000">
       <Calendar
         className={style.calendar}
         calendarType={"US"}
-        value={date}
+        activeStartDate={new Date(2023, 1, 26)}
+        value={new Date(2023, 1, 26)}
         nextLabel={null}
         next2Label={null}
         prevLabel={null}
         prev2Label={null}
-        onClickMonth={null}
-        minDetail={"month"}
+        showNeighboringMonth={false}
+        view={"month"}
         formatDay={(locale, date) => date.getDate()}
       />
     </div>
--- src/components/MapPage/MapPage.jsx
+++ src/components/MapPage/MapPage.jsx
@@ -19,7 +19,7 @@ export default function MapPage() {
           "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3170.29922445904!2d127.08977855068387!3d37.382755342290835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b585c9382c72b%3A0xcb2a2b6905978d03!2z7ZWY64iE7IaM642U7Z6Q!5e0!3m2!1sko!2skr!4v1672471415955!5m2!1sko!2skr"
         }
       ></embed>
-
+      <h3 className={style.location_name}>하누소 The Hill</h3>
       <div className={style.mapinfo} data-aos="fade-up" ata-aos-duration="3000">
         <p className={style.address}>
           경기 성남시 분당구 판교백현로 65 <br />
